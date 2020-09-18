437e7ed167aad6a4d4a6ece862567912987c55b0
nicholaspung <nicholaspungdev@gmail.com>
2020-09-18 11:48:31 -0700

    Added a manual API call button to check if backend is working as intended

--- src/components/HabitTracker/index.js
+++ src/components/HabitTracker/index.js
@@ -1,5 +1,26 @@
 import React from "react";
+import axios from "axios";
+import { getIdToken } from "../../firebase/utils";
 
-const HabitTracker = () => <div>Habit Tracker</div>;
+const HabitTracker = () => {
+  const fetchCall = async () => {
+    try {
+      const token = await getIdToken();
+      const response = await axios.get("http://127.0.0.1:8000/api/users/", {
+        headers: {
+          authorization: token,
+        },
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div>
+      Habit Tracker<button onClick={fetchCall}>Call</button>
+    </div>
+  );
+};
 
 export default HabitTracker;
