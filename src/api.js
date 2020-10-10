c0cd62744c109cab25d3d3207c92520d6c49ef5d
nicholaspung <nicholaspungdev@gmail.com>
2020-10-09 21:54:10 -0700

    Added a config for api endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import { getIdToken } from './firebase/utils';
 
-const baseUrl = 'http://127.0.0.1:8000/api';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.API_ROUTE
+    : 'http://127.0.0.1:8000/api';
 
 export const axiosWithAuth = async () => {
   const token = await getIdToken();
