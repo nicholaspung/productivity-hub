4d82e40ff0ba25ea7622f3f1ae446843d7eb3c08
nicholaspung <nicholaspungdev@gmail.com>
2020-10-14 22:17:40 -0700

    Wrong equality sign

--- src/store.js
+++ src/store.js
@@ -14,6 +14,6 @@ const combinedReducers = combineReducers({
 export default createStore(
   combinedReducers,
   process.env.NODE_ENV === 'production'
-    ? composeWithDevTools(applyMiddleware(thunk))
-    : applyMiddleware(thunk),
+    ? applyMiddleware(thunk)
+    : composeWithDevTools(applyMiddleware(thunk)),
 );
